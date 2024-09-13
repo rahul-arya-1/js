2c472b241ed8317f9fdfe26e27444d72565c6a71
Rahul <149096716+rahul-arya-1@users.noreply.github.com>
2024-09-13 20:06:25 +0000

    age adult

--- js-basic/1.js
+++ js-basic/1.js
@@ -1,2 +1,10 @@
 console.log("Rahul is a good man")
 console.log("My age is 19 years !")
+
+let age = 19;
+if (age > 18){
+    console.log("You are Adult !")
+}
+else{
+    console.log("You are not Adult !")
+}
